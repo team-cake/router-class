2e91a25deab1b0405230e5b9805fdc7777bfc86f
team-cake <eka.cheuk@gmail.com>
2020-08-05 16:41:51 +0200

    Code simplified

--- instantiate.js
+++ instantiate.js
@@ -5,17 +5,15 @@ const { Router } = express
 const router = new Router()
 
 const app = express()
-
 app.use(router)
 
 const port = 4000
 
 router.get('/hi/:name', async (req, res, next) => {
-        const name = req.params.name
-		res.send(`Hi! ${name}`)
+	const name = req.params.name
+	res.send(`Hi! ${name}`)
 })
 
 app.listen(port)
 
 module.exports = router
-
